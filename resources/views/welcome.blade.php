a590871c80584ce33ed04a6e666514d12e8a238a
Robert Marinho <robstermarinho@gmail.com>
2019-11-26 18:20:44 -0300

    Testing new changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testin AWS CodeBuild
                 </div>
 
                 <div class="links">
